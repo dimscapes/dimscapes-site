680aa83dc9b85dfc78363556343aa6346bd77f2d
dim <72107949+Dimssen@users.noreply.github.com>
2024-09-17 06:40:53 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,9 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <span className="theme-bejamas" />
+      <Head>
+        <link rel="icon" href="/favicon.png" />
+      </Head>
       <Component {...pageProps} />
     </>
   );
